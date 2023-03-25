f41b08700dff28178613b80b1a110a374c417804
Knii7 <ephraimnkd@gmail.com>
2023-03-24 18:34:20 -0700

    I am trying

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,4 +1,4 @@
-include "function_pointers.h"
+#include "function_pointers.h"
 /**
 * int_index - return index place if comparison = true, else -1
 * @array: array
@@ -6,6 +6,7 @@ include "function_pointers.h"
 * @cmp: pointer to func of one of the 3 in main
 * Return: 0
 */
+
 	int int_index(int *array, int size, int (*cmp)(int))
 	{
 	int i;
